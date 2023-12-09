c4bed4958e5e6ca4cf010e33586878f38ed6a21a
Matheus Brian <matheusbriandeoliveira@gmail.com>
2023-12-09 13:25:02 -0300

    Primeiro commit

---
+++ objeto.js
@@ -0,0 +1,10 @@
+const ListaCPFs = [1111111, 22222, 3333];
+
+const infoPessoa = ["nome", "Jose", "Idade", 32, "CPF", "1111222333"];
+
+console.log(infoPessoa[1])
+
+const objetoPessoa = {
+    nome: "Jose",
+    idade: 32,
+};
