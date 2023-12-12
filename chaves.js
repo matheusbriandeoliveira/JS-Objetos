b0ebbedc8731c5933fc8d84bca94b762b807666b
Matheus Brian <matheusbriandeoliveira@gmail.com>
2023-12-12 17:39:38 -0300

    finalizado

--- chaves.js
+++ chaves.js
@@ -1,24 +1,23 @@
 const cliente = {
-    nome: "João",
-    idade: 24,
-    email: "joao@firma.com",
-    telefone: ["115555500", "114444440"],
-};
+  nome: 'João',
+  idade: 24,
+  email: 'joao@firma.com',
+  telefone: ['115555500', '114444440']
+}
 
 cliente.enderecos = [
-    {
-        rua: "R. Joseph Climber",
-        numero: "1337",
-        apartamento: true,
-        complemento: "ap 934",
-    }
-];
+  {
+    rua: 'R. Joseph Climber',
+    numero: '1337',
+    apartamento: true,
+    complemento: 'ap 934'
+  }
+]
 
-const chavesDoObjeto = Object.keys(cliente);
+const chavesDoObjeto = Object.keys(cliente)
 
 console.log(chavesDoObjeto)
 
-if (!chavesDoObjeto.includes("enderecos")){
-    console.error("Erro. É necessário ter um endereço cadastrado.")
-
+if (!chavesDoObjeto.includes('enderecos')) {
+  console.error('Erro. É necessário ter um endereço cadastrado.')
 }
--- funcoes.js
+++ funcoes.js
@@ -1,18 +1,17 @@
 const cliente = {
-    nome: "João",
-    idade: 24,
-    email: "joao@firma.com",
-    telefone: ["115555500", "114444440"],
-    saldo: 200,
-    efetuaPagamento: function(valor) {
-        if (valor > this.saldo) {
-            console.log("Saldo insuficiente");
-        }
-        else {
-            this.saldo -= valor;
-            console.log(`Pagamento realizado. Novo saldo ${this.saldo}`);
-        }
+  nome: 'João',
+  idade: 24,
+  email: 'joao@firma.com',
+  telefone: ['115555500', '114444440'],
+  saldo: 200,
+  efetuaPagamento: function (valor) {
+    if (valor > this.saldo) {
+      console.log('Saldo insuficiente')
+    } else {
+      this.saldo -= valor
+      console.log(`Pagamento realizado. Novo saldo ${this.saldo}`)
     }
-};
+  }
+}
 
 cliente.efetuaPagamento(55)
